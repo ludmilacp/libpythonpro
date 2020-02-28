8ff5636b44ee5343201ef15431668ccbcdaf962f
ludmilacp <ludmila.canfield@gmail.com>
2020-02-28 13:46:20 -0300

    testes de envio de spam

---
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,38 @@
+import pytest
+
+from libpythonpro.spam.enviador_de_email import Enviador, EmailInvalido
+
+
+def test_criar_enviador_de_email():
+    enviador= Enviador()
+    assert enviador is not None
+
+@pytest.mark.parametrize(
+    'destinatario',
+    ['foo@bar.com.br', 'ludmila.canfield@gmail.com']
+)
+def test_remetente(destinatario):
+    enviador=Enviador()
+    resultado=enviador.enviar(
+        destinatario,
+        'luciacanfield@gmail.com',
+        'Cursos Python Pro',
+        'Turma Pytools'
+     )
+    assert destinatario in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'ludmila']
+)
+def test_remetente_invalido(remetente):
+    enviador=Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'luciacanfield@gmail.com',
+            'Cursos Python Pro',
+            'Turma Pytools'
+        )
+
