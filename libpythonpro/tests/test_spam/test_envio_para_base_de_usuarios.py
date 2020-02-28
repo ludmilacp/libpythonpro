1957e714720b6a8532fa67b48a320498adf78b95
ludmilacp <ludmila.canfield@gmail.com>
2020-02-28 19:20:14 -0300

    mock implementado nos testes

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,6 +1,7 @@
+from unittest.mock import Mock
+
 import pytest
 
-from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
 from libpythonpro.spam.modelos import Usuario
 
@@ -15,7 +16,17 @@ from libpythonpro.spam.modelos import Usuario
 def test_qde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails('renzo@python.pro.br', 'curso Python Pro', 'Confira os módulos fantasticos')
-    assert len(usuarios) == enviador.qtd_email_enviados
+    assert len(usuarios) == enviador.enviar.call_count
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Renzo', email='renzo@python.pro.br')
+    sessao.salvar(usuario)
+    enviador = Mock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails('luciano@python.pro.br', 'curso Python Pro', 'Confira os modulos fantasticos')
+    enviador.enviar.assert_called_once_with('luciano@python.pro.br', 'renzo@python.pro.br', 'curso Python Pro',
+                                            'Confira os modulos fantasticos')
