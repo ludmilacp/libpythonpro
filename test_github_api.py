1813747226185deea77f119dbce4f860d99d21d4
ludmilacp <ludmila.canfield@gmail.com>
2020-03-04 19:06:48 -0300

    implementado pytest-mock

--- test_github_api.py
+++ test_github_api.py
@@ -1,12 +1,26 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libpythonpro import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url(mocker):
     resp_mock = Mock()
+    url = 'https://avatars2.githubusercontent.com/u/60018104?v=4'
     resp_mock.json.return_value = {'login': 'ludmilacp', 'id': 60018104,
-                                   'avatar_url': 'https://avatars2.githubusercontent.com/u/60018104?v=4'}
-    github_api.requests.get = Mock(return_value=resp_mock)
+                                   'avatar_url': url}
+    get_mock = mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
+
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('ludmilacp')
+    assert 'https://avatars2.githubusercontent.com/u/60018104?v=4' == url
+
+
+def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('ludmilacp')
     assert 'https://avatars2.githubusercontent.com/u/60018104?v=4' == url
